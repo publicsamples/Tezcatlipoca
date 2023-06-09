e0333f90a76ff572e9ca252efef2208775c81e7e
Rick <53375860+modularsamples@users.noreply.github.com>
2023-06-09 12:22:56 +0100

    remove old networks

--- AdditionalSourceCode/nodes/includes.h
+++ AdditionalSourceCode/nodes/includes.h
@@ -7,35 +7,12 @@
 #pragma clang diagnostic ignored "-Wunused-variable"
 #endif
 
-// Include third party header files ----------------
-
-#include "g.h"
-#include "granular.h"
-#include "khp.h"
-#include "klp.h"
-
 // Include compiled network files ------------------
 
-#include "clone_networkdata.h"
-#include "delay_networkdata.h"
 #include "envin_networkdata.h"
-#include "Filter.h"
-#include "follower_networkdata.h"
-#include "Gran.h"
-#include "Granulator_networkdata.h"
-#include "jdel.h"
 #include "mod_networkdata.h"
-#include "pitchshifter.h"
 #include "postfollow_networkdata.h"
-#include "Switch_networkdata.h"
-#include "wdelaymod_networkdata.h"
-#include "wdelay_networkdata.h"
-#include "wdeleay2mod_networkdata.h"
-#include "wdeleay2_networkdata.h"
-#include "wdeleay3_networkdata.h"
-#include "wdeleay_networkdata.h"
-#include "WeirdDelay_networkdata.h"
-#include "_4WaySwitch_networkdata.h"
+#include "replace_networkdata.h"
 
 #if (defined (_WIN32) || defined (_WIN64))
 #pragma warning( pop )
---
+++ DspNetworks/Binaries/Builds/MacOSX/build/Tezcatlipoca.build/Release/Tezcatlipoca - Dynamic Library.build/Objects-normal/x86_64/RNBO.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/rick/Music/Audio\ Music\ Apps/Tezcatlipoca/Tezcatlipoca/DspNetworks/Binaries/Source/RNBO.cpp
